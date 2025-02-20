622ec22b7fed8a9d1ebd0692f109bc65f0d87f75
studentaccount2028 <tpark2028@kehillahstudent.org>
2025-02-20 10:59:27 -0800

    done!

--- q8.py
+++ q8.py
@@ -4,7 +4,23 @@ starts with one of these letters: c, t,, b
 has one of these letters in the middle: a, o
 ends with one of these letters: p, t, n
 
+
+
+
 (This should print out 18 words in total)
 """
 
 # YOUR CODE HERE
+
+start_letters = "ctb"
+middle_letters = "ao"
+end_letters = "ptn"
+counter = 0
+
+for first_letter in start_letters:
+    for middle_letter in middle_letters:
+        for last_letter in end_letters:
+            word = first_letter + middle_letter + last_letter
+            print(word)
+            counter+=1
+            print(counter)
